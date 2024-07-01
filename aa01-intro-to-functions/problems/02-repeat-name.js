48a1a30bcc5b26706ca2249c1a54c093592b9d56
Your Name <Your Email>
2024-07-01 14:21:32 -0400

    finished

---
+++ aa01-intro-to-functions/problems/02-repeat-name.js
@@ -0,0 +1,24 @@
+/*
+Assign the variables repeatDaniel, repeatMark, and repeatBeyonce to values
+by calling the repeatName(name) function so that the console.logs will
+print out the expected values below.
+*/
+
+function repeatName(name) {
+  return name + " " + name;
+}
+
+// Your code here
+let repeatDaniel = repeatName('Daniel');
+let repeatMark = repeatName('Mark');
+let repeatBeyonce = repeatName('Beyonce');
+// let repeatDaniel;
+// let repeatMark;
+// let repeatBeyonce;
+
+console.log(repeatDaniel);  // => "Daniel Daniel"
+console.log(repeatMark);    // => "Mark Mark"
+console.log(repeatBeyonce); // => "!!!Beyonce Beyonce!!!"
+
+/******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
+module.exports = repeatName;
